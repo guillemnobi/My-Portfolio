bd0c1ca17be1e3f3a87e26d3ee768f8cc7d5e20c
guillemnobi <guillemguillem01@gmail.com>
2021-10-10 20:45:07 +0200

    changes

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY>50){
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+
+    }
+  });
+
+  // toggle menu/navbar
+  $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
+  
+
+
+
+
+
+  
+});
