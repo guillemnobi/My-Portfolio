0fe05733b994226da9aeccb6c10da26c4074d702
guillemnobi <guillemguillem01@gmail.com>
2021-10-17 14:13:49 +0200

    changes 1710 aos added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     if(this.scrollY > 500){
       $('.scroll-up-btn').addClass("show");
     } else {
-      $('.scroll-up-btn').removeClass("show");
+ 
     }
   });
 
